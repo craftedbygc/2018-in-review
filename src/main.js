9fed310338fca83bd7d0b56102632bad013ed844
Ash Thornton <ash@craftedbygc.com>
2019-02-13 11:58:06 +0000

    animations

--- src/main.js
+++ src/main.js
@@ -6,24 +6,24 @@ if (module.hot) {
     module.hot.accept()
 
     module.hot.dispose(() => {
-        window.assets = timeline.assets
-        timeline.renderer.domElement.removeEventListener('wheel', timeline.scroll)
-        timeline.renderer.domElement.removeEventListener('resize', timeline.resize)
-        timeline.renderer.domElement.removeEventListener('mousedown', timeline.mouseDown)
-        timeline.renderer.domElement.removeEventListener('mouseup', timeline.mouseUp)
-        removeEventListener('mousemove', timeline.mouseMove)
-        document.querySelector('canvas').remove()
-        timeline.renderer.forceContextLoss()
-        timeline.renderer.context = null
-        timeline.renderer.domElement = null
-        timeline.renderer = null
-        cancelAnimationFrame(timeline.animationId)
+        // window.assets = timeline.assets
+        // timeline.renderer.domElement.removeEventListener('wheel', timeline.scroll)
+        // timeline.renderer.domElement.removeEventListener('resize', timeline.resize)
+        // timeline.renderer.domElement.removeEventListener('mousedown', timeline.mouseDown)
+        // timeline.renderer.domElement.removeEventListener('mouseup', timeline.mouseUp)
+        // removeEventListener('mousemove', timeline.mouseMove)
+        // document.querySelector('canvas').remove()
+        // timeline.renderer.forceContextLoss()
+        // timeline.renderer.context = null
+        // timeline.renderer.domElement = null
+        // timeline.renderer = null
+        // cancelAnimationFrame(timeline.animationId)
 
-        timeline.gesture.destroy()
+        // timeline.gesture.destroy()
     })
 }
 
 
 
-const timeline = new Timeline()
-window.timeline = timeline
+// const timeline = new Timeline()
+// window.timeline = timeline
